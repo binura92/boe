682656c9123f95faef9f5db7ca7de1fce25d6c0a
AmilaSankha <amilasankha@gmail.com>
2016-01-06 11:48:00 +0530

    no message

--- js/script.js
+++ js/script.js
@@ -50,87 +50,6 @@ $( document ).ready(function( $ ) {
 });
 
 
-function loadMsgInbox(){
-	var str = document.getElementById("selectMsg").value;
-	//alert(str);
-	var httpxml;	
-	httpxml=new XMLHttpRequest();
-	function stateChanged() {
-    	if(httpxml.readyState==4){
-			document.getElementById("InboxView").innerHTML=httpxml.responseText;
-			document.getElementById("msg").style.display='none';
-      	}
-    }
-	var url="searchMsg.php";
-	url=url+"?txt="+str;
-	url=url+"&sid="+Math.random();
-	httpxml.onreadystatechange=stateChanged;
-	httpxml.open("GET",url,true);
-	httpxml.send(null);
-	//document.getElementById("msg").innerHTML="Please Wait ...";
-	
-	//document.getElementById("msg").style.display='inline';
-	
-	
-}
-
-function loadMsgOutbox(){
-	var str = document.getElementById("selectMsgOutbox").value;
-	//alert(str);
-	var httpxml;	
-	httpxml=new XMLHttpRequest();
-	function stateChanged() {
-    	if(httpxml.readyState==4){
-			document.getElementById("OutboxView").innerHTML=httpxml.responseText;
-			document.getElementById("msg").style.display='none';
-      	}
-    }
-	var url="searchOutboxMsg.php";
-	url=url+"?txt="+str;
-	url=url+"&sid="+Math.random();
-	httpxml.onreadystatechange=stateChanged;
-	httpxml.open("GET",url,true);
-	httpxml.send(null);
-	//document.getElementById("msg").innerHTML="Please Wait ...";
-	
-	//document.getElementById("msg").style.display='inline';
-	
-	
-}
-
-function ajaxFunction(str){
-	document.getElementById("dis").innerHTML = "<div id='displayDiv' ></div>";
-	var httpxml;	
-	httpxml=new XMLHttpRequest();
-	
-	function stateChanged() {
-    	if(httpxml.readyState==4){
-			document.getElementById("displayDiv").innerHTML=httpxml.responseText;
-			document.getElementById("msg").style.display='none';
-      	}
-    }
-	var url="searchFriend.php";
-	url=url+"?txt="+str;
-	url=url+"&sid="+Math.random();
-	httpxml.onreadystatechange=stateChanged;
-	httpxml.open("GET",url,true);
-	httpxml.send(null);
-	document.getElementById("msg").innerHTML="Please Wait ...";
-	document.getElementById("msg").style.display='inline';
-}
-
-	function setValue(){
-		var ID_name = document.getElementById("select").value;
-		var pos1 = ID_name.indexOf("@");
-		var pos2 = ID_name.indexOf("@@")
-		var ID = ID_name.substring(0,pos1);
-		var name = ID_name.substring(pos1+1,pos2)+" "+ID_name.substring(pos2+2)
-		document.getElementById("searchFriend").value = name;
-		document.getElementById("result").value = ID;
-		document.getElementById("displayDiv").style.display = "none";
-		
-	}
-	
 	
 function sendMsg(){
 	var rName = document.getElementById("Rname").value;
@@ -151,12 +70,16 @@ function sendMsg(){
 			ajax.onreadystatechange = function() {
 				if(ajaxReturn(ajax) == true){
 					if(ajax.responseText != 1){
-						//alert(ajax.responseText);
 						alert("Message sending failed");
 					}else{
 						window.scrollTo(0,0);
 						alert("Message sent successfully");
-						$( "#NewMessage" ).css("visibility",'hidden');
+                                                /*
+                                                document.getElementById("Rname").innerHTML = "";
+                                                document.getElementById("R_ID").innerHTML = "";
+                                                document.getElementById("Title").innerHTML = "";
+                                                document.getElementById("msgBody").innerHTML = "";
+                                                */
 					}
 				}	
 			}
@@ -165,12 +88,85 @@ function sendMsg(){
 	}
 }
 	
+
+
 	
-function mSearchFriend(str){
-	document.getElementById("disFriend").innerHTML = "<div id='displayDiv' ></div>";
+function inbox(){
 	var httpxml;	
 	httpxml=new XMLHttpRequest();
+	function stateChanged() {
+    	if(httpxml.readyState==4){
+			document.getElementById("InboxList").innerHTML=httpxml.responseText;
+      	}
+    }
+	var url="inboxView.php";
+	url=url+"?txt=";
+	url=url+"&sid="+Math.random();
+	httpxml.onreadystatechange=stateChanged;
+	httpxml.open("GET",url,true);
+	httpxml.send(null);
+}
+
+function outbox(){
+	var httpxml;	
+	httpxml=new XMLHttpRequest();
+	function stateChanged() {
+    	if(httpxml.readyState==4){
+			document.getElementById("OutboxList").innerHTML=httpxml.responseText;
+      	}
+    }
+	var url="outboxView.php";
+	url=url+"?txt=";
+	url=url+"&sid="+Math.random();
+	httpxml.onreadystatechange=stateChanged;
+	httpxml.open("GET",url,true);
+	httpxml.send(null);
 	
+}
+
+
+function loadMsgInbox(){
+	var str = document.getElementById("selectMsg").value;
+	var httpxml;	
+	httpxml=new XMLHttpRequest();
+	function stateChanged() {
+    	if(httpxml.readyState==4){
+			document.getElementById("InboxView").innerHTML=httpxml.responseText;
+			document.getElementById("msg").style.display='none';
+      	}
+    }
+	var url="searchMsg.php";
+	url=url+"?txt="+str;
+	url=url+"&sid="+Math.random();
+	httpxml.onreadystatechange=stateChanged;
+	httpxml.open("GET",url,true);
+	httpxml.send(null);
+}
+
+function loadMsgOutbox(){
+	var str = document.getElementById("selectMsgOutbox").value;
+	var httpxml;	
+	httpxml=new XMLHttpRequest();
+	function stateChanged() {
+    	if(httpxml.readyState==4){
+			document.getElementById("OutboxView").innerHTML=httpxml.responseText;
+			document.getElementById("msg").style.display='none';
+      	}
+    }
+	var url="searchOutboxMsg.php";
+	url=url+"?txt="+str;
+	url=url+"&sid="+Math.random();
+	httpxml.onreadystatechange=stateChanged;
+	httpxml.open("GET",url,true);
+	httpxml.send(null);
+}
+
+
+
+function ajaxFunction(str){
+	document.getElementById("dis").innerHTML = "<div id='displayDiv' ></div>";
+	var httpxml;	
+	httpxml=new XMLHttpRequest();
 	function stateChanged() {
     	if(httpxml.readyState==4){
 			document.getElementById("displayDiv").innerHTML=httpxml.responseText;
@@ -187,17 +183,56 @@ function mSearchFriend(str){
 	document.getElementById("msg").style.display='inline';
 }
 
-	function setValue(){
-		var ID_name = document.getElementById("select").value;
-		var pos1 = ID_name.indexOf("@");
-		var pos2 = ID_name.indexOf("@@")
-		var ID = ID_name.substring(0,pos1);
-		var name = ID_name.substring(pos1+1,pos2)+" "+ID_name.substring(pos2+2)
-		document.getElementById("Rname").value = name;
-		document.getElementById("R_ID").value = ID;
-		document.getElementById("displayDiv").style.display = "none";
-		
-	}
+
+function setValue1(){
+	var ID_name = document.getElementById("select").value;
+	var pos1 = ID_name.indexOf("@");
+	var pos2 = ID_name.indexOf("@@")
+	var ID = ID_name.substring(0,pos1);
+	var name = ID_name.substring(pos1+1,pos2)+" "+ID_name.substring(pos2+2)
+	document.getElementById("searchFriend").value = name;
+	document.getElementById("result").value = ID;
+	document.getElementById("displayDiv").style.display = "none";
+        loadProfile(ID);
+}
 	
+
 	
+function mSearchFriend(str){
+	document.getElementById("disFriend").innerHTML = "<div id='displayDiv' ></div>";
+	var httpxml;	
+	httpxml=new XMLHttpRequest();
 	
+	function stateChanged() {
+    	if(httpxml.readyState==4){
+			document.getElementById("displayDiv").innerHTML=httpxml.responseText;
+			document.getElementById("msg").style.display='none';
+      	}
+    }
+	var url="searchFriend2.php";
+	url=url+"?txt="+str;
+	url=url+"&sid="+Math.random();
+	httpxml.onreadystatechange=stateChanged;
+	httpxml.open("GET",url,true);
+	httpxml.send(null);
+	document.getElementById("msg").innerHTML="Please Wait ...";
+	document.getElementById("msg").style.display='inline';
+}
+
+function setValue2(){
+	var ID_name = document.getElementById("select").value;
+	var pos1 = ID_name.indexOf("@");
+	var pos2 = ID_name.indexOf("@@")
+	var ID = ID_name.substring(0,pos1);
+	var name = ID_name.substring(pos1+1,pos2)+" "+ID_name.substring(pos2+2)
+	document.getElementById("Rname").value = name;
+	document.getElementById("R_ID").value = ID;
+	document.getElementById("displayDiv").style.display = "none";
+	
+}
+
+function loadProfile(id){
+    //alert(id);
+    window.location = "friendProfile.php?u="+id;
+}
+
