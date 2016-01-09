6194553b1e911b7676fc4ce28533f1ad5a040a17
AmilaSankha <amilasankha@gmail.com>
2016-01-09 21:58:50 +0530

    no message

--- php/searchFriend2.php
+++ php/searchFriend2.php
@@ -1,27 +1,24 @@
 <?Php
 
 require "DB_Connection.php";  // database connection
-
 //////////////////////////////// Main Code sarts /////////////////////////////////////////////
 
 
-$in=$_GET['txt'];
-if(!ctype_alnum($in)){
-	echo "Data Error";
-exit;
+$in = $_GET['txt'];
+if (!ctype_alnum($in)) {
+    echo "Data Error";
+    exit;
 }
 
-$msg="";
-$msg="<select id='select' size='15' name='selt'>";
-if(strlen($in)>0 and strlen($in) <20 ){
-$sql="select First_Name, Last_Name, Registation_ID from registered_user where First_Name like '%$in%' or Last_Name like '%$in%'";
-foreach ($dbo->query($sql) as $nt) {
-	$rValue = $nt['Registation_ID']."@".$nt['First_Name'].'@@'.$nt['Last_Name'];
-//$msg.=$nt[name]."->$nt[id]<br>";
-$msg .="<option id='search_option' value=$rValue onClick='setValue2();'> $nt[First_Name] $nt[Last_Name] </option>";
-//$msg .="<option value='$nt[name]'>";
+$msg = "";
+$msg = "<select id='select' size='15' name='selt'>";
+if (strlen($in) > 0 and strlen($in) < 20) {
+    $sql = "select First_Name, Last_Name, Registation_ID from registered_user where First_Name like '%$in%' or Last_Name like '%$in%'";
+    foreach ($dbo->query($sql) as $nt) {
+        $rValue = $nt['Registation_ID'] . "@" . $nt['First_Name'] . '@@' . $nt['Last_Name'];
 
-}
+        $msg .="<option id='search_option2' value=$rValue onClick='setValue2();'> $nt[First_Name] $nt[Last_Name] </option>";
+    }
 }
 $msg .='</select>';
 echo $msg;
