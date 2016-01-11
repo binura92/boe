d4438ca4ce86fe61edc456b2944f9924df5cd214
AmilaSankha <amilasankha@gmail.com>
2016-01-11 16:21:39 +0530

    no message

--- php/newComment.php
+++ php/newComment.php
@@ -4,6 +4,7 @@ session_start();
 $ID = $_SESSION['id'];
 $storyID = $_POST['story_ID'];
 $comment =$_POST['comment'];
+$comment = mysql_real_escape_string($comment);
 
 $sql = "INSERT INTO story_comment (Story_ID, Author_ID, Comment) VALUES ('$storyID','$ID','$comment');";
 $sql2 = "UPDATE story SET lastUpdate = now()";
