904c668fcb94339eeddbb9062e07eced1f5220d1
AmilaSankha <amilasankha@gmail.com>
2016-01-11 21:36:57 +0530

    ALTER TABLE `story` ADD `view` INT(1) NOT NULL DEFAULT '1' ;

--- php/StoryCategoryPage.php
+++ php/StoryCategoryPage.php
@@ -64,7 +64,7 @@ if (isset($_SESSION['login']) && isset($_GET["cid"])) {
         echo("<h3>" . $scategory . "<h3/>");
         echo("<hr><br>");
 
-        $sql = "SELECT Story_ID FROM story WHERE Category_ID='$cid' and (Type != 'pr' or Author_ID = '$id') ORDER BY Publish_Date DESC";
+        $sql = "SELECT Story_ID FROM story WHERE (Category_ID='$cid' and (Type != 'pr' or Author_ID = '$id')and view= 1) ORDER BY Publish_Date DESC";
         $result = $con->query($sql);
         if ($result->num_rows > 0) {
             // output data of each row
