d69b02bfd7ce2f4e75b6ca6c6d5bc3f8430ba32b
Binura Dodangoda <bdodangoda@gmail.com>
2016-01-11 14:06:02 +0530

    no message

--- php/searchFriend.php
+++ php/searchFriend.php
@@ -20,7 +20,7 @@ if (strlen($in) > 0 and strlen($in) < 20) {
     $sql = "select First_Name, Last_Name, Registation_ID from registered_user where First_Name like '%$in%' or Last_Name like '%$in%'";
     foreach ($dbo->query($sql) as $nt) {
         $rValue = $nt['Registation_ID'] . "@" . $nt['First_Name'] . '@@' . $nt['Last_Name'];
-        $msg .="<option id='search_option1' value=$rValue onClick='setValue1();'>$nt[First_Name] $nt[Last_Name] </option>";
+        $msg .="<option id='search_option1' value=$rValue onClick='setValue1();'> $nt[First_Name] $nt[Last_Name] </option>";
     }
 }
 $msg .='</select>';
