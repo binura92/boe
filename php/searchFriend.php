1e1471b3d33442a35568ab1ad84f2fa779822733
AmilaSankha <amilasankha@gmail.com>
2016-01-11 12:59:36 +0530

    no message

--- php/searchFriend.php
+++ php/searchFriend.php
@@ -20,7 +20,8 @@ if (strlen($in) > 0 and strlen($in) < 20) {
     $sql = "select First_Name, Last_Name, Registation_ID from registered_user where First_Name like '%$in%' or Last_Name like '%$in%'";
     foreach ($dbo->query($sql) as $nt) {
         $rValue = $nt['Registation_ID'] . "@" . $nt['First_Name'] . '@@' . $nt['Last_Name'];
-        $msg .="<option id='search_option1' value=$rValue onClick='setValue1();'>$nt[First_Name] $nt[Last_Name] </option>";
+        $profilePic = $nt['Registation_ID'].".jpg" ;
+        $msg .="<option id='search_option1' style='background-image:url(../profilePic/$profilePic);background-size: auto 30px;background-position: right; background-repeat: no-repeat;' value=$rValue onClick='setValue1();'>$nt[First_Name] $nt[Last_Name]</option> ";
     }
 }
 $msg .='</select>';
@@ -28,7 +29,3 @@ $msg .='<input type="text" id="user_ID" value="' . $id . '" style="visibility: h
 echo $msg;
 //echo "<img src='../profilePic/1.jpg'/>";
 ?>
-
-<select>
-    <option value="1"></option>
-</select>
