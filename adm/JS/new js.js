0fc11659a8af383866887939eb7085cbffb0ab01
AmilaSankha <amilasankha@gmail.com>
2016-01-12 17:33:29 +0530

    no message

--- adm/JS/new js.js
+++ adm/JS/new js.js
@@ -1,8 +1,8 @@
 var cAdmin=document.getElementById("cAdmin");
 cAdmin.onclick=fcAdmin;
 
-functio fcAdmin(){
-	document.getElementById("main").innerHTML='<object type="type/html" data="cAdmin.php" ></object>'
+function fcAdmin(){
+	document.getElementById("main").innerHTML='<object type="type/html" data="cAdmin.php" ></object>';
 	
 	}
 
