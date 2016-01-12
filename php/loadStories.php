13f5ac133bfe87e0b481d881ddd7b2a90f0ffc3c
Binura Dodangoda <bdodangoda@gmail.com>
2016-01-12 18:56:36 +0530

    no message

--- php/loadStories.php
+++ php/loadStories.php
@@ -11,14 +11,18 @@ $result = mysqli_query($con, $sql3);
 $i = 1;
 while ($row = mysqli_fetch_assoc($result)) {
     $storyID = $row["Story_ID"];
+    $catID = $row["Category_ID"];
+    $sqlFindCat = "SELECT Category_Title FROM category WHERE Category_ID = $catID";
+    $queryFindCat = mysqli_query($con, $sqlFindCat);
+    $resFindCat = mysqli_fetch_assoc($queryFindCat);
     $author = $row['First_Name'] . ' ' . $row['Last_Name'];
     $title = $row['Title'];
-
+    $catName = $resFindCat["Category_Title"];
 
     echo "<main id='acc'>
                 <section id='item." . $i . "'>
-                <a href='#item." . $i . "' style='font-family:Verdana, Geneva, sans-serif' onclick =viewStory($storyID) >" . $title . "</a>
-                <div id='postDet'>Posted by <b>" . $author . "</b></div>
+                <a href='#item." . $i . "' style='font-family:Verdana, Geneva, sans-serif' onclick =viewStory($storyID) ><b>" . $title . "</b></a>
+                <div id='postDet'>Posted by <b>" . $author . "</b><br><a href='StoryCategoryPage.php?cid=$catID'>".$catName."</a></br></div>
               
                     <hr/>
                     
