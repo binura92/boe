abe4280f6e768b68b380815095f566a16f46470a
Charitha Sampath Gunawardana <charitha009@gmail.com>
2016-01-13 08:05:20 +0530

    Warning page

--- php/warning.php
+++ php/warning.php
@@ -1,23 +1,27 @@
 <html>
     <head>
         <title>.:BE:.</title>
+        <link href="../css/inquiry.css" type="text/css" rel="stylesheet">
     </head>
-    <body>
+    <body>        
         <?php
         session_start();
         $id = $_SESSION['id'];
-        ?>
-        <?php
         include_once("template_top.php");
-        $con = mysqli_connect("localhost", "root", "", "bookofexperiences");
-        $sql2 = "SELECT* FROM warning WHERE Receiver='$id'";
-        $query = mysqli_query($con, $sql2);
-        $row = mysqli_fetch_assoc($query);
-        $title = $row['Title'];
-        $description = $row['Description'];
-        echo("<h2> Title : " . $title . "</h2>");
-        echo ("<h3> Description : " .$description ."</h3>");
         ?>
-
+        <div id="mainbucketinc">
+            <div class="warningshowbuket">
+                <?php
+                $con = mysqli_connect("localhost", "root", "", "bookofexperiences");
+                $sql2 = "SELECT* FROM warning WHERE Receiver='$id'";
+                $query = mysqli_query($con, $sql2);
+                $row = mysqli_fetch_assoc($query);
+                $title = $row['Title'];
+                $description = $row['Description'];
+                echo("<h2 class='warningleft'> Title : </h2><h2 class='warningright'> " . $title . "</h2><br>");
+                echo ("<h2 class='warningleft'> Description : </h2><h2 class='warningright'>" . $description ."</h2><br>");
+                ?>
+            </div>
+        </div>
     </body>
 </html>
